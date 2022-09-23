f8565ab114b5688ab5be7c5ba824033fc67864db
Harsha Kumar <harzkr142@gmail.com>
2022-09-23 10:28:06 +0530

    Testing random generation

--- pages/api/generate-code.js
+++ pages/api/generate-code.js
@@ -1,8 +1,22 @@
 
 
+const runTester = () => {
+  console.time('runTester');
+  for(let i=268435456; i < 16**8; i++) {
+    //console.log(i.toString(16).toUpperCase());
+    if(i === 16**8 - 1) {
+      console.log(i.toString(16).toUpperCase())
+      console.log('doing last element')
+    }
+  }
+  console.timeEnd('runTester');
+}
+
 export default function handler(req, res) {
 
   const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+
+  runTester();
 
   res.status(200).json({ code: genRanHex(8) });
 }
