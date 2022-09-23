570d37eaeef5ec8dbff98bb6543177ed7fb3d843
Harsha Kumar <harzkr142@gmail.com>
2022-09-23 09:24:42 +0530

    Updates to design pages

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <div
         style={{
           display: "flex",
-          marginTop: "2rem",
+          marginTop: "4rem",
         }}
       >
         <Typography
@@ -39,10 +39,31 @@ export default function Home() {
         </Grid>
       </div>
 
-      <div>
+      <div
+        style={{
+          marginTop: "4rem",
+        }}
+      >
         <Button variant="contained">
-          <Typography variant="h5">Generate</Typography>
+          <Typography variant="h6">Generate</Typography>
         </Button>
+      </div>
+
+      <div>
+        <Typography
+          variant="h6"
+          style={{
+            marginTop: "4rem",
+            whiteSpace: "pre-line",
+            textAlign: "center",
+          }}
+        >
+          Every time the program runs it will emit one 8-digit hexadecimal code;{'\n'}
+          It will emit every possible code before repeating;{'\n'} It will not print
+          "odd-looking" codes such as 0xAAAAAAAA or 0x01234567{'\n'} or any commonly
+          used words, phrases, or hexspeak such as 0xDEADBEEF;{'\n'} Codes will be
+          emitted in apparently random order.
+        </Typography>
       </div>
     </div>
   );
