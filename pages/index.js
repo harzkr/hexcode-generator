1bb91155b24f07acce6b4dc0d841b9ff8a6b7567
Harsha Kumar <harzkr142@gmail.com>
2022-09-23 09:32:32 +0530

    Update display styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import React from "react";
 import styles from "../styles/Home.module.css";
 import { Typography, Button, Grid, Paper } from "@mui/material";
 
 export default function Home() {
+  const [code, setCode] = React.useState("00000000");
+
+  const generateNewCode = () => {
+    console.log("Generating new code");
+
+    fetch("/api/generate-code")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+
+        setCode(data.code);
+      });
+  };
+
   return (
     <div className={styles.container}>
       <Typography variant="h3" component="h1" gutterBottom>
@@ -25,14 +40,26 @@ export default function Home() {
         </Typography>
         <Grid item xs={12}>
           <Grid container justifyContent="center" spacing={0.5}>
-            {[0, 1, 2, 3, 4, 5, 6, 7].map((value) => (
-              <Grid key={value} item>
+            {code.split("").map((value, index) => (
+              <Grid key={index} item>
                 <Paper
                   sx={{
                     height: 100,
                     width: 100,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
                   }}
-                />
+                >
+                  <Typography
+                    style={{
+                      textAlign: "center",
+                    }}
+                    variant="h2"
+                  >
+                    {value}
+                  </Typography>
+                </Paper>
               </Grid>
             ))}
           </Grid>
@@ -44,7 +71,7 @@ export default function Home() {
           marginTop: "4rem",
         }}
       >
-        <Button variant="contained">
+        <Button variant="contained" onClick={() => generateNewCode()}>
           <Typography variant="h6">Generate</Typography>
         </Button>
       </div>
@@ -58,11 +85,12 @@ export default function Home() {
             textAlign: "center",
           }}
         >
-          Every time the program runs it will emit one 8-digit hexadecimal code;{'\n'}
-          It will emit every possible code before repeating;{'\n'} It will not print
-          "odd-looking" codes such as 0xAAAAAAAA or 0x01234567{'\n'} or any commonly
-          used words, phrases, or hexspeak such as 0xDEADBEEF;{'\n'} Codes will be
-          emitted in apparently random order.
+          Every time the program runs it will emit one 8-digit hexadecimal code;
+          {"\n"}
+          It will emit every possible code before repeating;{"\n"} It will not
+          print "odd-looking" codes such as 0xAAAAAAAA or 0x01234567{"\n"} or
+          any commonly used words, phrases, or hexspeak such as 0xDEADBEEF;
+          {"\n"} Codes will be emitted in apparently random order.
         </Typography>
       </div>
     </div>
