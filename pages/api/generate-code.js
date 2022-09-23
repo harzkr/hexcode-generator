52939850e80b96cc6aed3d6126324c9ebc1b1151
Harsha Kumar <harzkr142@gmail.com>
2022-09-23 14:54:21 +0530

    Randomise redis selection

--- pages/api/generate-code.js
+++ pages/api/generate-code.js
@@ -1,34 +1,50 @@
 import { client } from '../../lib/redis';
+let start_counter = 268435456;
+let current_counter = 268445456;
+const end_counter = 16**8;
+
+let current_keys = [];
+
 
 const resetRedis = () => {
-  client.flushall();
+  client.flushAll();
 }
 
 const validCode = (code) => {
   return true;
 };
 
+const fetchCode = async () => {
+
+  const allCodes = await client.hGetAll('codes');
+
+  const allKeys = Object.keys(allCodes);
+
+  const randomKey = allKeys[Math.floor(Math.random() * allKeys.length)];
+
+  const code = allCodes[randomKey];
+
+  return code;
+};
+
 const populateRedis = async () => {
-  for(let i=1; i < 10; i++) {
+  for(let i=start_counter; i < current_counter; i++) {
     const currCode = i.toString(16).toUpperCase();
     if(validCode(currCode)) {
-      await client.hSet('codes', currCode, 1);
-    }
-    if(i === 16**8 - 1) {
-      console.log(currCode)
-      console.log('doing last element')
+      await client.hSet('codes', i, currCode);
     }
   }
 
-  const res = await client.hGetAll('codes');
-  console.log(res);
-}
+  console.log('done populating redis');
+  start_counter = current_counter;
+  current_counter = current_counter + 10000;
+};
 
 const runTester = () => {
   console.time('runTester');
-  for(let i=268435456; i < 16**8; i++) {
-    //console.log(i.toString(16).toUpperCase());
-    if(i === 16**8 - 1) {
+  for(let i=268435456; i < 268445456; i++) {
+    console.log(i.toString(16).toUpperCase());
+    if(i === 268445456 - 1) {
       console.log(i.toString(16).toUpperCase())
       console.log('doing last element')
     }
@@ -36,12 +52,17 @@ const runTester = () => {
   console.timeEnd('runTester');
 }
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
 
-  const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
-
-  populateRedis();
+  //await resetRedis();
+  //populateRedis();
   //console.log(client);
+  //resetRedis();
+  //runTester();
 
-  res.status(200).json({ code: genRanHex(8) });
+  const fetchedCode = await fetchCode();
+
+  console.log('final fetched code', fetchedCode);
+
+  res.status(200).json({ code: fetchedCode });
 }
