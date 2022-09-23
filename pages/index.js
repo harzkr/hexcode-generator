0840f320c66d03765cadbb55342e5e87bc3f2f2a
Harsha Kumar <harzkr142@gmail.com>
2022-09-23 09:20:32 +0530

    Add grid design

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import styles from '../styles/Home.module.css'
-import { Typography } from '@mui/material'
+import styles from "../styles/Home.module.css";
+import { Typography, Button, Grid, Paper } from "@mui/material";
 
 export default function Home() {
   return (
@@ -7,6 +7,43 @@ export default function Home() {
       <Typography variant="h3" component="h1" gutterBottom>
         Hexadecimal Code Generator
       </Typography>
+
+      <div
+        style={{
+          display: "flex",
+          marginTop: "2rem",
+        }}
+      >
+        <Typography
+          style={{
+            marginRight: "1rem",
+            marginTop: "1rem",
+          }}
+          variant="h2"
+        >
+          0x
+        </Typography>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing={0.5}>
+            {[0, 1, 2, 3, 4, 5, 6, 7].map((value) => (
+              <Grid key={value} item>
+                <Paper
+                  sx={{
+                    height: 100,
+                    width: 100,
+                  }}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </div>
+
+      <div>
+        <Button variant="contained">
+          <Typography variant="h5">Generate</Typography>
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
