0a3f1f277dc1d79bca35ff516221bbfb836a4d0d
Harsha Kumar <harzkr142@gmail.com>
2022-09-23 09:12:28 +0530

    Update index page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import styles from '../styles/Home.module.css'
+import { Typography } from '@mui/material'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      Hexadecimal Code Generator
+      <Typography variant="h3" component="h1" gutterBottom>
+        Hexadecimal Code Generator
+      </Typography>
     </div>
   )
 }
