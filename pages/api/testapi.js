f550b349e2806274f2bd03827cb20ab3ebb5c9ff
Harsha Kumar <harzkr142@gmail.com>
2022-09-24 07:55:05 +0530

    Add comments

--- pages/api/testapi.js
+++ pages/api/testapi.js
@@ -1,35 +1,33 @@
 //Custom testing API call
 
 const test = async () => {
-    try{
-        const res = await fetch("http://localhost:3001/api/generate-code");
-        const data = await res.json();
-        return data.code
-    } catch(err){
-        console.log(err);
-    }
-}
-
+  try {
+    const res = await fetch("http://localhost:3001/api/generate-code");
+    const data = await res.json();
+    return data.code;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export default async function handler(req, res) {
+  //Please note, the script will take around 3-4 minutes to run
+  const arr = Array.from(Array(100000).keys());
+  const arr_check = [];
 
-    const arr = Array.from(Array(100000).keys());
-    const arr_check = []
+  console.log(arr.length, "initial array length check");
 
-    console.log(arr.length, 'initial array length check');
+  for (const elem of arr) {
+    const c = await test();
 
+    arr_check.push(c);
+  }
 
-    for(const elem of arr){
-        const c = await test();
+  const hasDuplicates = (_arr) => _arr.length !== new Set(_arr).size;
 
-        arr_check.push(c);
-    }
+  console.log(arr_check.length, "codes array length check");
 
-    const hasDuplicates = (_arr) => _arr.length !== new Set(_arr).size;
+  const testRes = hasDuplicates(arr_check);
 
-    console.log(arr_check.length, 'codes array length check');
-
-    const testRes = hasDuplicates(arr_check);
-
-    res.status(200).json({ result:testRes });
+  res.status(200).json({ result: testRes });
 }
